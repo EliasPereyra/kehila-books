996f1be24805edfb155643f94b2ca5903034ad2c
Elias Pereyra Gomez <EliasPereyra_Gomez@hotmail.com>
2023-09-26 23:56:11 -0300

    Create a utlity folder

---
+++ src/utility/Books.ts
@@ -0,0 +1,59 @@
+interface Book {
+  title: string;
+  desc: string;
+  cover_img: string;
+  book_link: string;
+  author: string;
+}
+
+export const books: Book[] = [
+  {
+    title: "Estudios de Torah",
+    desc: "",
+    cover_img: "https://res.cloudinary.com/dnjbeodgl/image/upload/v1695155976/kehila-books-cover/portada-estudios-de-torah-819x1024_fyfkja.jpg",
+    book_link: "/assets/books/estudios-torah.pdf",
+    author: "Dr. Javier Palacios Celorio"
+  },
+  {
+    title: "Liberación Demoníaca",
+    desc: "",
+    cover_img: "https://res.cloudinary.com/dnjbeodgl/image/upload/v1695157357/kehila-books-cover/liberacion-cover_ysvsvk.jpg",
+    book_link: "https://tinyurl.com/gypliberdem",
+    author: "Dr. Javier Palacios Celorio"
+  },
+  {
+    title: "Pasos para ser un talmid de Y'HM",
+    desc: "",
+    cover_img: "https://res.cloudinary.com/dnjbeodgl/image/upload/v1695157219/kehila-books-cover/talmid-cover_m5emdx.jpg",
+    book_link: "https://tinyurl.com/pasotalmid",
+    author: "Dr. Javier Palacios Celorio"
+  },
+  {
+    title: "¿Quién es Y'HM?",
+    desc: "",
+    cover_img: "https://res.cloudinary.com/dnjbeodgl/image/upload/v1695157240/kehila-books-cover/quien-coverjpg_z6jq91.jpg",
+    book_link: "https://tinyurl.com/YHMespanol",
+    author: "Dr. Javier Palacios Celorio"
+  },
+  {
+    title: "20 Características de un salvo",
+    desc: "",
+    cover_img: "https://res.cloudinary.com/dnjbeodgl/image/upload/v1695157275/kehila-books-cover/salvo-cover_oplusg.jpg",
+    book_link: "https://tinyurl.com/20caractgyp",
+    author: "Dr. Javier Palacios Celorio"
+  },
+  {
+    title: "Aprendiendo a vivir",
+    desc: "",
+    cover_img: "https://res.cloudinary.com/dnjbeodgl/image/upload/v1695157255/kehila-books-cover/aprendiendo-cover_qsosxp.jpg",
+    book_link: "https://tinyurl.com/aprendvivirgyp",
+    author: "Dr. Javier Palacios Celorio"
+  },
+  {
+    title: "Señales de los Últimos Tiempos",
+    desc: "",
+    cover_img: "https://res.cloudinary.com/dnjbeodgl/image/upload/v1695157255/kehila-books-cover/aprendiendo-cover_qsosxp.jpg",
+    book_link: "https://tinyurl.com/aprendvivirgyp",
+    author: "Dr. Javier Palacios Celorio"
+  }
+]
