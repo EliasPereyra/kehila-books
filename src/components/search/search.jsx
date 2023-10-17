d7c57d3380a4517c727e827d24b3d1dbd2aa4b98
Elias Pereyra Gomez <EliasPereyra_Gomez@hotmail.com>
2023-10-16 23:48:02 -0300

    Add some more tags below the input component

--- src/components/search/search.jsx
+++ src/components/search/search.jsx
@@ -1,5 +1,16 @@
+import { useState } from "react";
 import "./styles.css";
 
 export function SearchInput() {
-  return <input className="input" type="text" placeholder="Buscar..." />;
+  const [title, setTitle] = useState("")
+
+  const tags = ["torah", "tanaj", "brithadasha", "profecias", "sanidad"]
+
+  return <div className="input-container">
+    <input className="input" type="text" placeholder="Buscar..." value={title} onChange={(e) => {setTitle(e.target.value)}} />
+    <ul class="tags">
+      {tags.map((tag, i) => <small key={i}>{`#${tag}`}</small>)}
+    </ul>
+    {title}
+  </div>;
 }
